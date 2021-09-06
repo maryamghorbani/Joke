221eee2c7b799090800d9d5e18ce0f79d694b19a
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-09-06 15:22:54 +0430

    show 5 jokes randomely

--- src/Components/CardBody.js
+++ src/Components/CardBody.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React , { useState , useEffect } from 'react';
 
 
 // import css files
 import './CSS/Card.css'
+import axios from "axios";
 
 
 function CardBody() {
+
+    const [responseJoke , setResponseJoke] = useState([]);
+    const getDta = () => {
+        // GET request to get random joke
+        axios.get('https://api.chucknorris.io/jokes/random')
+            .then((response) => {
+                const myJoke = response.data;
+                setResponseJoke(myJoke);
+                // pass data response to state
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+    useEffect(() => getDta(), [])
+
     return (
         <p className="card-body">
-            HI ...
+            {responseJoke.value}
         </p>
     )
 }
