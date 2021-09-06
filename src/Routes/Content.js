c980592cce219bcbf1922045d2db3769598a2a87
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-09-06 12:35:07 +0430

    get 3 random category

--- src/Routes/Content.js
+++ src/Routes/Content.js
@@ -1,4 +1,4 @@
-import React , { useEffect } from 'react';
+import React , { useEffect , useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from "axios";
 
@@ -9,20 +9,26 @@ import '../Components/CSS/Content.css'
 function Content() {
 
 
+    const [responseData , setResponseData] = useState([]);
     const getDta = () => {
         // GET request to get random category
         axios.get('https://api.chucknorris.io/jokes/categories')
             .then((response) => {
-                console.log(response)
+                const myData = response.data;
+                setResponseData(myData);
+                // pass data response to state
             })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
     useEffect(() => getDta(), [])
 
     return (
         <div className="box-container">
-            <div className="box"><Link className="link" to="/route1">Route1</Link></div>
-            <div className="box"><Link className="link" to="/route2">Route2</Link></div>
-            <div className="box"><Link className="link" to="/route3">Route3</Link></div>
+            <div className="box"><Link className="link" to="/route1">{responseData[Math.floor(Math.random() * responseData.length)]}</Link></div>
+            <div className="box"><Link className="link" to="/route2">{responseData[Math.floor(Math.random() * responseData.length)]}</Link></div>
+            <div className="box"><Link className="link" to="/route3">{responseData[Math.floor(Math.random() * responseData.length)]}</Link></div>
         </div>
     )
 }
